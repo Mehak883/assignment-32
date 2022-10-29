6cde514eec1d37e892acb6290c8fa3c31d06d211
Mehak883 <108986900+Mehak883@users.noreply.github.com>
2022-10-29 18:40:08 +0530

    Add files via upload

---
+++ assign32q10.cpp
@@ -0,0 +1,30 @@
+// Create a C++ class for player objects with the following attributes: player no., name, number of matches and number of goals done in 
+// each match. The number of matches varies for each player. Write a parameterized constructor which initializes player no., name, number
+// of subjects and creates an array for number of goals and number of matches dynamically.
+#include<iostream>
+using namespace std;
+class player{
+int player_no;
+char name[10];
+int number_of_matches;
+int goals[10];
+public:
+player(){};
+player(int p_no,char n[],int num_matches,int goals[]){
+
+
+
+}
+};
+int main(){
+    player *p1=new player();
+    int *goals
+   int player_name;
+char name[10];
+int number_of_matches;
+int goals[10];
+for(int i=0;i<3;i++){
+p1[i]->
+} 
+    return 0;
+}
